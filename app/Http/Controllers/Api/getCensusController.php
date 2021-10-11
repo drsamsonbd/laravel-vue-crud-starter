fd6eeb584e99cccb8e01d360fd9097a9cab2d7a4
drsamsonbd <drsamsonbd@gmail.com>
2021-10-11 15:59:09 +0800

    Update getCensusController.php

--- app/Http/Controllers/Api/getCensusController.php
+++ app/Http/Controllers/Api/getCensusController.php
@@ -51,8 +51,94 @@ $admission= DB::table('admissions')
  });
 $kelmarin_adult_female_pkrc = $statistic_adult_female_pkrc->count();
 
+//PAEDS male
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','12')
+->where('patients.age', '>=','1')
+->where('patients.gender', '=','LELAKI');
+
+ $statistic_paeds_male_pkrc =  $admission ->where('admissions.date', '<', $request -> datereporting)
+ ->where('discharges.date_dc', '>', $request -> datereporting)
+ ->orWhere(function($admission )use ($request)  
+ { 
+     $admission
+     ->where('patients.gender', '=','LELAKI')
+     ->where('admissions.date', '<', $request -> datereporting)
+      ->where('discharges.reg_number')
+      ->where('patients.age', '<','12')
+      ->where('patients.age', '>=','1');
+ });
+$kelmarin_paeds_male_pkrc = $statistic_paeds_male_pkrc->count();
+
+
+
+ //PAEDS female
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','12')
+->where('patients.age', '>=','1')
+->where('patients.gender', '=','PEREMPUAN');
+
+ $statistic_paeds_female_pkrc =  $admission ->where('admissions.date', '<', $request -> datereporting)
+ ->where('discharges.date_dc', '>', $request -> datereporting)
+ ->orWhere(function($admission )use ($request)  
+ { 
+     $admission
+     ->where('patients.gender', '=','PEREMPUAN')
+     ->where('admissions.date', '<', $request -> datereporting)
+      ->where('discharges.reg_number')
+      ->where('patients.age', '<','12')
+      ->where('patients.age', '>=','1');
+ });
+
+$kelmarin_paeds_female_pkrc = $statistic_paeds_female_pkrc->count();
+
+
+
+//NB male
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','1')
+->where('patients.gender', '=','LELAKI');
+
+ $statistic_newborn_male_pkrc =  $admission ->where('admissions.date', '<', $request -> datereporting)
+ ->where('discharges.date_dc', '>', $request -> datereporting)
+ ->orWhere(function($admission )use ($request)  
+ { 
+     $admission
+     ->where('patients.gender', '=','LELAKI')
+     ->where('admissions.date', '<', $request -> datereporting)
+      ->where('discharges.reg_number')
+      ->where('patients.age', '<','1');
+ });
+$kelmarin_newborn_male_pkrc = $statistic_newborn_male_pkrc->count();
+
+ //NB female
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','1')
+->where('patients.gender', '=','PEREMPUAN');
+
+ $statistic_newborn_female_pkrc =  $admission ->where('admissions.date', '<', $request -> datereporting)
+ ->where('discharges.date_dc', '>', $request -> datereporting)
+ ->orWhere(function($admission )use ($request)  
+ { 
+     $admission
+     ->where('patients.gender', '=','PEREMPUAN')
+     ->where('admissions.date', '<', $request -> datereporting)
+      ->where('discharges.reg_number')
+      ->where('patients.age', '<','1');
+ });
+$kelmarin_newborn_female_pkrc = $statistic_newborn_female_pkrc->count();
+
 
 //INPATIENT
+
 //total_male
          $datatable= DB::table('bed__disciplines')
 
@@ -122,10 +208,16 @@ $kelmarin_adult_female_pkrc = $statistic_adult_female_pkrc->count();
       $count_transfer_discipline_total_male=  $stat_transfer_discipline_total_male ->count();
       
      $ward = $request ->ward_id;
+
       if ($ward == 1){
             $kelmarin_total_male = $count_kelmarin_total_male - $count_transfer_ward_total_male - $count_transfer_discipline_total_male + $kelmarin_adult_male_pkrc ;
         
       }
+elseif($ward == 7){
+      $kelmarin_total_male = $count_kelmarin_total_male - $count_transfer_ward_total_male - $count_transfer_discipline_total_male + $kelmarin_paeds_male_pkrc + $kelmarin_newborn_male_pkrc ;
+  
+}
+
 else {
       $kelmarin_total_male = $count_kelmarin_total_male - $count_transfer_ward_total_male - $count_transfer_discipline_total_male ;
 }
@@ -165,6 +257,8 @@ $stat_kelmarin_total_female =  $datatable
 
 $count_kelmarin_total_female = $stat_kelmarin_total_female -> count();
 
+
+
 $data_transfer= DB::table('bed__disciplines')
 
 ->join('ward_admissions','bed__disciplines.rn','ward_admissions.reg_number')
@@ -193,7 +287,21 @@ $stat_transfer_discipline_total_female =  $data_transfer->where([['bed__discipli
 
 $count_transfer_ward_total_female=  $stat_transfer_ward_total_female ->count();
 $count_transfer_discipline_total_female=  $stat_transfer_discipline_total_female ->count();
+
+if ($ward == 6){
+      $kelmarin_total_female = $count_kelmarin_total_female - $count_transfer_ward_total_female - $count_transfer_discipline_total_female + $kelmarin_adult_female_pkrc ;
+  
+}
+elseif($ward == 7){
+$kelmarin_total_female = $count_kelmarin_total_female - $count_transfer_ward_total_female - $count_transfer_discipline_total_female + $kelmarin_paeds_female_pkrc + $kelmarin_newborn_female_pkrc ;
+
+}
+
+else {
 $kelmarin_total_female = $count_kelmarin_total_female - $count_transfer_ward_total_female - $count_transfer_discipline_total_female ;
+}
+
+
 
 
 
@@ -248,6 +356,9 @@ $stat_kelmarin_discipline_male =  $datatable
 });
 
 $count_kelmarin_discipline_male = $stat_kelmarin_discipline_male -> count();
+
+
+
 
         $data_transfer= DB::table('bed__disciplines')
         ->join('ward_admissions','bed__disciplines.rn','ward_admissions.reg_number')
@@ -334,12 +445,89 @@ $count_kelmarin_discipline_female = $stat_kelmarin_discipline_female -> count();
         
         $count_transfer_ward_discipline_female=  $stat_transfer_ward_total_female ->count();
         
+        $discipline = $request -> discipline_id;
+        if($discipline == 1 )  {
+          $kelmarin_discipline_female = $count_kelmarin_discipline_female - $count_transfer_ward_discipline_female + $kelmarin_adult_female_pkrc;
+        }
+        else{
+              $kelmarin_discipline_female = $count_kelmarin_discipline_female - $count_transfer_ward_discipline_female;
+          
+        }
         
-        $kelmarin_discipline_female = $count_kelmarin_discipline_female - $count_transfer_ward_discipline_female;
 
 
 // ADMISSION STAT
 
+//PKRC ADMISSION
+//adult male
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '>','12')
+->where('patients.gender', '=','LELAKI');
+
+ $statisticadmission_adult_male_pkrc =  $admission ->where('admissions.date', '=', $request -> datereporting);
+
+$admission_adult_male_pkrc = $statisticadmission_adult_male_pkrc->count();
+
+ //adult female
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '>','12')
+->where('patients.gender', '=','PEREMPUAN');
+
+ $statisticadmission_adult_female_pkrc = $admission ->where('admissions.date', '=', $request -> datereporting);
+$admission_adult_female_pkrc = $statisticadmission_adult_female_pkrc->count();
+
+//PAEDS male
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','12')
+->where('patients.age', '>=','1')
+->where('patients.gender', '=','LELAKI');
+
+ $statisticadmission_paeds_male_pkrc = $admission ->where('admissions.date', '=', $request -> datereporting);
+$admission_paeds_male_pkrc = $statisticadmission_paeds_male_pkrc->count();
+
+
+
+ //PAEDS female
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','12')
+->where('patients.age', '>=','1')
+->where('patients.gender', '=','PEREMPUAN');
+
+ $statisticadmission_paeds_female_pkrc =$admission ->where('admissions.date', '=', $request -> datereporting);
+
+$admission_paeds_female_pkrc = $statisticadmission_paeds_female_pkrc->count();
+
+
+
+//NB male
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','1')
+->where('patients.gender', '=','LELAKI');
+
+ $statisticadmission_newborn_male_pkrc = $admission ->where('admissions.date', '=', $request -> datereporting);
+$admission_newborn_male_pkrc = $statisticadmission_newborn_male_pkrc->count();
+
+ //NB female
+$admission= DB::table('admissions')
+->join('patients','admissions.kp_passport','patients.kp_passport')
+->leftjoin('discharges','admissions.reg_number','=','discharges.reg_number')
+->where('patients.age', '<','1')
+->where('patients.gender', '=','PEREMPUAN');
+
+ $statisticadmission_newborn_female_pkrc = $admission ->where('admissions.date', '=', $request -> datereporting);
+$admission_newborn_female_pkrc = $statisticadmission_newborn_female_pkrc->count();
+
+//INPATIENT ADMISSION
 //male
 $data_transfer= DB::table('bed__disciplines')
 
@@ -355,7 +543,15 @@ $stat_admission_total_male =  $data_transfer->where([['bed__disciplines.remarks'
 ['bed__disciplines.date_bed', '=', $request -> datereporting]])
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_adult_total_male=  $stat_admission_total_male ->count();
+$count_admission_adult_total_male=  $stat_admission_total_male ->count();
+
+if ($ward == 1){
+      $admission_adult_total_male = $count_admission_adult_total_male + $admission_adult_male_pkrc ;  
+}
+
+else {   $admission_adult_total_male = $count_admission_adult_total_male ;
+}
+
 //female
 $tblAdmFemale= DB::table('bed__disciplines')
 
@@ -371,7 +567,15 @@ $stat_admission_total_female =  $tblAdmFemale->where([['bed__disciplines.remarks
 ['bed__disciplines.date_bed', '=', $request -> datereporting]])
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_adult_total_female=  $stat_admission_total_female ->count();
+$count_admission_adult_total_female=  $stat_admission_total_female ->count();
+if ($ward == 7){
+      $admission_adult_total_female = $count_admission_adult_total_female + $admission_adult_female_pkrc ;  
+}
+
+else {   $admission_adult_total_female = $count_admission_adult_total_female ;
+}
+
+
 $admission_adult_total_total =$admission_adult_total_female+$admission_adult_total_male;
 
 //discipline male
@@ -390,7 +594,17 @@ $stat_admission_discipline_male =  $data_transfer->where([['bed__disciplines.rem
 ->where('bed__disciplines.discipline_id', '=', $request -> discipline_id)
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_adult_discipline_male=  $stat_admission_discipline_male ->count();
+$count_admission_adult_discipline_male=  $stat_admission_discipline_male ->count();
+
+$discipline = $request -> discipline_id;
+if($discipline == 1 )  {
+  $admission_adult_discipline_male = $count_admission_adult_discipline_male + $admission_adult_male_pkrc;
+}
+else{
+      $admission_adult_discipline_male = $count_admission_adult_discipline_male;
+  
+}
+
 //discipline female
 $tblAdmFemale= DB::table('bed__disciplines')
 
@@ -407,7 +621,17 @@ $stat_admission_discipline_female =  $tblAdmFemale->where([['bed__disciplines.re
 ->where('bed__disciplines.discipline_id', '=', $request -> discipline_id)
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_adult_discipline_female=  $stat_admission_discipline_female ->count();
+$count_admission_adult_discipline_female=  $stat_admission_discipline_female ->count();
+
+$discipline = $request -> discipline_id;
+if($discipline == 1 )  {
+  $admission_adult_discipline_female = $count_admission_adult_discipline_female + $admission_adult_female_pkrc;
+}
+else{
+      $admission_adult_discipline_female = $count_admission_adult_discipline_female;
+  
+}
+
 
 //paed stat
 // paeds  male
@@ -427,7 +651,14 @@ $stat_admission_total_male =  $data_transfer->where([['bed__disciplines.remarks'
 ['bed__disciplines.date_bed', '=', $request -> datereporting]])
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_paeds_total_male=  $stat_admission_total_male ->count();
+$count_admission_paeds_total_male=  $stat_admission_total_male ->count();
+if ($ward == 7){
+      $admission_paeds_total_male = $count_admission_paeds_total_male + $admission_paeds_male_pkrc ;  
+}
+
+else {   $admission_paeds_total_male = $count_admission_paeds_total_male ;
+}
+
 // paeds  female
 $tblAdmFemale= DB::table('bed__disciplines')
 
@@ -444,7 +675,14 @@ $stat_admission_total_female =  $tblAdmFemale->where([['bed__disciplines.remarks
 ['bed__disciplines.date_bed', '=', $request -> datereporting]])
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_paeds_total_female=  $stat_admission_total_female ->count();
+$count_admission_paeds_total_female=  $stat_admission_total_female ->count();
+if ($ward == 7){
+      $admission_paeds_total_female = $count_admission_paeds_total_female + $admission_paeds_female_pkrc ;  
+}
+
+else {   $admission_paeds_total_female = $count_admission_paeds_total_female ;
+}
+
 $admission_paeds_total_total =$admission_paeds_total_female+$admission_paeds_total_male;
 
 //discipline paeds male
@@ -464,7 +702,16 @@ $stat_admission_discipline_male =  $data_transfer->where([['bed__disciplines.rem
 ->where('bed__disciplines.discipline_id', '=', $request -> discipline_id)
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_paeds_discipline_male=  $stat_admission_discipline_male ->count();
+$count_admission_paeds_discipline_male=  $stat_admission_discipline_male ->count();
+
+$discipline = $request -> discipline_id;
+if($discipline == 1 )  {
+  $admission_paeds_discipline_male = $count_admission_paeds_discipline_male + $admission_paeds_male_pkrc;
+}
+else{
+      $admission_paeds_discipline_male = $count_admission_paeds_discipline_male;
+  
+}
 //discipline paeds female
 $tblAdmFemale= DB::table('bed__disciplines')
 
@@ -482,7 +729,18 @@ $stat_admission_discipline_female =  $tblAdmFemale->where([['bed__disciplines.re
 ->where('bed__disciplines.discipline_id', '=', $request -> discipline_id)
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_paeds_discipline_female=  $stat_admission_discipline_female ->count();
+$count_admission_paeds_discipline_female=  $stat_admission_discipline_female ->count();
+
+$discipline = $request -> discipline_id;
+if($discipline == 1 )  {
+  $admission_paeds_discipline_female = $count_admission_paeds_discipline_female + $admission_paeds_female_pkrc;
+}
+else{
+      $admission_paeds_discipline_female = $count_admission_paeds_discipline_female;
+  
+}
+
+
 
 
 //newborn stat
@@ -504,7 +762,17 @@ $stat_admission_total_male =  $data_transfer->where([['bed__disciplines.remarks'
 ['bed__disciplines.date_bed', '=', $request -> datereporting]])
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_newborn_total_male=  $stat_admission_total_male ->count();
+
+$count_admission_newborn_total_male=  $stat_admission_total_male ->count();
+if ($ward == 7){
+      $admission_newborn_total_male = $count_admission_newborn_total_male + $admission_newborn_male_pkrc ;  
+}
+
+else {   $admission_newborn_total_male = $count_admission_newborn_total_male ;
+}
+
+
+
 // newborn  female
 $tblAdmFemale= DB::table('bed__disciplines')
 
@@ -520,7 +788,17 @@ $stat_admission_total_female =  $tblAdmFemale->where([['bed__disciplines.remarks
 ['bed__disciplines.date_bed', '=', $request -> datereporting]])
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_newborn_total_female=  $stat_admission_total_female ->count();
+
+$count_admission_newborn_total_female=  $stat_admission_total_female ->count();
+if ($ward == 7){
+      $admission_newborn_total_female = $count_admission_newborn_total_female + $admission_newborn_female_pkrc ;  
+}
+
+else {   $admission_newborn_total_female = $count_admission_newborn_total_female ;
+}
+
+
+
 $admission_newborn_total_total =$admission_newborn_total_female+$admission_newborn_total_male;
 
 //discipline newborn male
@@ -539,7 +817,20 @@ $stat_admission_discipline_male =  $data_transfer->where([['bed__disciplines.rem
 ->where('bed__disciplines.discipline_id', '=', $request -> discipline_id)
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_newborn_discipline_male=  $stat_admission_discipline_male ->count();
+$count_admission_newborn_discipline_male=  $stat_admission_discipline_male ->count();
+
+$discipline = $request -> discipline_id;
+if($discipline == 1 )  {
+  $admission_newborn_discipline_male = $count_admission_newborn_discipline_male + $admission_newborn_male_pkrc;
+}
+else{
+      $admission_newborn_discipline_male = $count_admission_newborn_discipline_male;
+  
+}
+
+
+
+
 //discipline newborn female
 $tblAdmFemale= DB::table('bed__disciplines')
 
@@ -556,7 +847,18 @@ $stat_admission_discipline_female =  $tblAdmFemale->where([['bed__disciplines.re
 ->where('bed__disciplines.discipline_id', '=', $request -> discipline_id)
 ->where('beds.ward_id', '=', $request -> ward_id);
 
-$admission_newborn_discipline_female=  $stat_admission_discipline_female ->count();
+$count_admission_newborn_discipline_female=  $stat_admission_discipline_female ->count();
+
+$discipline = $request -> discipline_id;
+if($discipline == 1 )  {
+  $admission_newborn_discipline_female = $count_admission_newborn_discipline_female + $admission_newborn_female_pkrc;
+}
+else{
+      $admission_newborn_discipline_female = $count_admission_newborn_discipline_female;
+  
+}
+
+
 
 
 // TRANSFER WARD
